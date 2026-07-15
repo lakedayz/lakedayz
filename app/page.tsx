6e1338415045d4ffba33ff3294ead1a1c4e51a14
Tucker Rees <tuckerrees@Tuckers-MBP.lan>
2026-07-14 19:00:09 -0500

    Add navbar to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Categories from "./components/CategoryCards";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Hero />
       <Categories />
     </main>
